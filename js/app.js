9edf344814242d38f8e9cdc9a757c87b620b9914
Nithish sv <nithishsudarsome@gmail.com>
2020-06-12 20:09:40 +0530

    testing js

---
+++ js/app.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+
+    angular.module('BlogApp', [])
+        .controller('BlogAppController', BlogAppController)
+        .service('BlogGetService', BlogGetService)
+        .constant('ApiBasePath', "https://github.com/Curiosity-431/Blog-Api/blob/master");
+       // .directive('BlogItems', BlogItemsDirective);
+
+
+    BlogAppController.$inject = ['BlogGetService'];
+
+    function BlogAppController(BlogGetService) {
+        var blog = this;
+
+        var promise = BlogGetService.getBlogItems();
+
+        promise.then(function (response) {
+                blog.quotes = response.data;
+            })
+            .catch(function (error) {
+                console.log("Something went terribly wrong.");
+            });
+
+
+    }
+
+    BlogGetService.$inject = ['$http', 'ApiBasePath'];
+
+    function BlogGetService($http, ApiBasePath) {
+        var service = this;
+
+        service.getBlogItems = function () {
+            var response = $http({
+                method: "GET",
+                url: (ApiBasePath + "/blog_quotes_api.json")
+            });
+
+            return response;
+        };
+    }
+
+
+
+
+
+})();
